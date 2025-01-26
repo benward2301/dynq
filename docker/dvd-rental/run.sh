e7db13f1169efb5b9086e893b5aa1144504bd201
Ben Ward <ben-ward@live.co.uk>
2025-01-26 00:14:38 +0000

    Compress dvd_rental volume

--- docker/dvd-rental/run.sh
+++ docker/dvd-rental/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+set -e
+dir=$(dirname "$(realpath $0)")
 
+echo n | unzip "$dir/volume.zip" -d "$dir" &> /dev/null
 docker rm -f dynamodb-local-dvdr &> /dev/null
 
 docker run -d \
   -u root \
   -p 8000:8000 \
-  -v "$(pwd)/docker/dvd-rental/volume:/home/dynamodblocal/data" \
+  -v "$dir/volume:/home/dynamodblocal/data" \
   -w /home/dynamodblocal \
   --name dynamodb-local-dvdr \
   amazon/dynamodb-local:latest -jar DynamoDBLocal.jar -sharedDb -dbPath ./data
